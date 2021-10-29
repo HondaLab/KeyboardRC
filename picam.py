956c9ba7ebcc283923d40ac2db82b66408e005ba
Masashi Yamada <mashi_16024166@yahoo.co.jp>
2021-10-29 12:15:13 +0900

    imshow 800x608

--- picam.py
+++ picam.py
@@ -19,7 +19,7 @@ class PI_CAMERA():
       #カメラを初期化，カメラへのアクセス？ルート？オブジェクト作成？
       self.cam = PiCamera()
       self.cam.framerate = 30  #フレームレート
-      self.cam.brightness = 60 #明るさ
+      self.cam.brightness = 50 #明るさ
       #cam.saturation = 50
 
       self.cam.awb_mode='auto'
@@ -55,7 +55,7 @@ if __name__ == "__main__":
     record='n' # if you want record movie, select 'y' 
                # and its FPS down to 10Hz.
                # if you select 'n' FPS is 30Hz without recording.
-    OUT_FILE="/tmp/output.mp4"
+    OUT_FILE="/tmp/output.avi"
 
     PERIOD=0.2 # Indication period
 
@@ -63,8 +63,8 @@ if __name__ == "__main__":
     print("# Captured movie is written in %s ." % OUT_FILE)
     fmt = cv2.VideoWriter_fourcc('m', 'p', '4', 'v')
     record_fps=10
-    width=320
-    height=320
+    width=800
+    height=608
     print("# Resolution: %5d x %5d" % (width,height))
     size = (width, height)
     vw = cv2.VideoWriter(OUT_FILE, fmt, record_fps, size)
@@ -90,7 +90,8 @@ if __name__ == "__main__":
         ch=key.read()
         try: 
             frame = cam.capture()
-            cv2.imshow("Front View", frame)
+            show=cv2.resize(frame,(800,608))
+            cv2.imshow("Front View", show)
             cv2.waitKey(1)
 
             if record=='y':
